d5449ee642138a067902912036c68a46958cd18e
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2025-06-12 16:52:03 -0300

    Primeiro commit

---
+++ Tentaiva de Senha.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <time.h>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+    string senha, senhad;
+    int tentativa;
+
+    senha = "algoritimo123";
+    tentativa = 1;
+
+    cout << "Digite a senha: ";
+    cin >> senhad;
+
+
+
+    while (senhad != senha and tentativa < 3) {
+        cout << "Tente novamente:";
+        cin >> senhad;
+        tentativa = tentativa + 1;
+    }
+        if (senhad == senha) {
+            cout << "Senha está correta";
+        }
+        else {
+            cout << "Você excedeu o limite de senhas possíveis!" << endl;
+        }
+
+
+    return 0;
+}
